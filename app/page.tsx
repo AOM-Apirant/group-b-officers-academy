677b42a974a554f9223ba0866831bc73875e28c8
Group-B-Officers-Academy <groupbofficersacademy@gmail.com>
2025-10-16 23:13:05 +0530

    Welcome

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>GROUP B OFFICERS ACADEMY</div>
+    <div className='text-2xl font-bold'>WELCOME TO GROUP B OFFICERS ACADEMY</div>
   )
 }
 
